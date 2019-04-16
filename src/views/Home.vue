0e7208c05127752745d63fa92f8f02792cf9e25f
Tim Whiting <tim@whitings.org>
2019-04-16 16:25:14 -0600

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main class="mainPageContent">
+    <h1>Public Recipes</h1>
+    <section class="masonry" id="recipeImages">
+      <figure v-for="recipe in recipes" v-bind:key="recipe._id">
+        <img v-bind:src="recipe.strMealThumb">
+        <figcaption>{{ recipe.strMeal }}</figcaption>
+      </figure>
+    </section>
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+const getRandomMealURL = "https://www.themealdb.com/api/json/v1/1/random.php";
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  name: "Home",
+  data: function() {
+    return {
+      recipes: []
+    };
+  },
+  created() {
+    this.getAllRecipes();
+  },
+  methods: {
+    async getAllRecipes() {
+      //get a random recipe
+      //format it
+      //add it to the container
+      let numImages = 15;
+      for (let i = 0; i < numImages; i++) {
+        console.log(getRandomMealURL);
+        let recipe = await axios.get(getRandomMealURL);
+        //console.log(recipe);
+        this.recipes.push(recipe.data.meals[0]);
+      }
+    }
   }
-}
+};
 </script>
+
+<style scoped>
+</style>

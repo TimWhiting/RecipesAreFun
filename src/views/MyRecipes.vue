f6f598392255f6aa5fa81ca9412e0ce2c4d00c77
Tim Whiting <tim@whitings.org>
2019-04-29 20:30:14 -0600

    updated

--- src/views/MyRecipes.vue
+++ src/views/MyRecipes.vue
@@ -96,8 +96,6 @@ export default {
   }
 };
 //Deleting fails to refresh page
-//Adding a recipe, the ingredients and probably instructions aren't an array
-//Images don't show up
 </script>
 
 <style scoped>
